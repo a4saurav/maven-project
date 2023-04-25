baf63418ba7cfd3ac4102ffd1b6c48b46f896a5c
avinash <a4saurav@gmail.com>
2023-04-25 18:25:11 +0530

    files added to git

---
+++ src/main/java/com/akshat/akshat_project/App.java
@@ -0,0 +1,13 @@
+package com.akshat.akshat_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
