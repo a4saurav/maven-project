cdb7d1e157710b727e12a2481d48fc0ba9209b29
avinash <a4saurav@gmail.com>
2023-04-30 20:43:42 +0530

    modification2

--- src/main/java/com/akshat/akshat_project/App.java
+++ src/main/java/com/akshat/akshat_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to java Programming" );
+        System.out.println( "Welcome to java Programming, This  is my first java code" );
     }
 }
