7ef39749e5a2c44cdd5253f418a13afd69f49226
avinash <a4saurav@gmail.com>
2023-05-25 01:46:24 +0530

    added new lines

--- src/main/java/com/akshat/akshat_project/App.java
+++ src/main/java/com/akshat/akshat_project/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to java Programming, This  is my first java code" );
+        System.out.println( "Welcome to java Programming, This  is my first java code, I have added some new lines. I love betu" );
     }
 }
